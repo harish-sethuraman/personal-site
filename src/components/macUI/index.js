cc59b0fce14248e81d389d5ed66c9086e234f28c
harish-sethuraman <harish.sethuraman@freshworks.com>
2021-06-19 21:24:11 +0530

    :heavy_plus_sign:  Update UI for mac

--- src/components/macUI/index.js
+++ src/components/macUI/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MacUI = () => (
   <div className="monitor-holder">
     <div className="monitor">
-      <iframe id="macos-clone" title="cloneverse demo" src="https://macos-clone.netlify.app" />
+      <iframe style={{ background: 'black' }} id="macos-clone" title="cloneverse demo" src="https://macos-clone.netlify.app" />
     </div>
     <div className="mac-chin" />
     <div className="mac-stand" />
